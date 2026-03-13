4ffc5a74d382585a3fed0f265760991e8b7e4ae5
AHearn <ahearn1@emich.edu>
2026-03-12 23:55:37 -0400

    Add simple break indicator

--- Pomodoro/pomodoro.py
+++ Pomodoro/pomodoro.py
@@ -4,13 +4,14 @@ from datetime import datetime, timedelta
 from tkinter import ttk,messagebox
 
 def set_countdown():
-    end_time = datetime.now() + timedelta(minutes = 25)
+    end_time = datetime.now() + timedelta(minutes = 5)
 
     def update():
         remaining = end_time - datetime.now()
 
         if remaining.total_seconds() <= 0:
             timer_var.set('00:00')
+            break_indicator.set("On break")
             return
 
         mins, secs = divmod(int(remaining.total_seconds()),60)
@@ -33,6 +34,9 @@ timer_var = StringVar(value="25:00")
 ttk.Label(mainframe,textvariable=timer_var).grid()
 ttk.Button(mainframe,text="start",command=set_countdown).grid()
 
+
+break_indicator =StringVar(value = "not on break")
+ttk.Label(mainframe,textvariable= break_indicator).grid()
 ttk.Button(mainframe, text="pause").grid()
 
 root.mainloop()
